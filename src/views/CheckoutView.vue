4d80f510e0d3c617c722207841333ee324e38e5e
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-12-13 23:07:31 +0100

    refactor: Rename checkout component to follow Vue's naming conventions

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import Checkout from '@/components/Checkout.vue'
+import TheCheckout from '@/components/TheCheckout.vue';
 </script>
 
 <template>
   <div class="checkout-view container mx-auto px-4 py-8">
-    <Checkout />
+    <TheCheckout />
   </div>
 </template>
